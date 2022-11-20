c315f20faa1a32b9ff630c85489a0fb7bcb85a74
Mustafa <mustafaislamovich@gmail.com>
2022-11-20 18:35:27 +0100

    lazy loading

--- pages/index.js
+++ pages/index.js
@@ -14,9 +14,12 @@ export default function Home() {
       </Head>
       <div className='bgWrap flex items-center justify-center'>
         <Image src='/../public/me.jpg' 
+        alt='me'
         width={350}
         height={350}
-        quality={100}/>
+        quality={100}
+        priority={true}
+        />
       </div>
       <Hero></Hero>
     </div>
